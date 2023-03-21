ff045b8d5f181ac2bf5a2c5162310e319f92f94b
‘CptKay’ <sammerz@bluewin.ch>
2023-03-21 16:57:28 +0100

    init 00

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MessageController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+/* Route::get('/', function () {
+    return view('welcome');
+}); */
+
+Route::get('/messages', [MessageController::class, 'showAll']);
+Route::get('/message/{id}', [MessageController::class, 'details']);
+
+
+Route::post('/create', [MessageController::class, 'create']);
+
+Route::delete('/message/{id}', [MessageController::class, 'delete']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
